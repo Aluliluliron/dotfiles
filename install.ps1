f8a999c9fc60416183e0f5083b33be4de8fac450
Aluliluliron <djedo.official@gmail.com>
2021-04-17 03:21:00 -0700

    Update

--- install.ps1
+++ install.ps1
@@ -1,9 +1,9 @@
 mkdir $HOME/.config
-function ln ($target, $link) { New-Item -Path $link -ItemType SymbolicLink -Value $target }
-ln $HOME/dotfiles/.config/vim $HOME/.config/vim
-ln $HOME/dotfiles/.config/poshthemes $HOME/.config/poshthemes
-ln $HOME/dotfiles/.config/thefuck $HOME/.config/thefuck
-ln $HOME/dotfiles/.config/colorls $HOME/.config/colorls
-ln $HOME/dotfiles/.config/nvim/init.vim $HOME/Appdata/Local/nvim/init.vim
-ln $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln $HOME/dotfiles/powershell/Microsoft.PowerShell_profile.ps1 $HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+function mklink ($target, $link) { New-Item -Path $link -ItemType SymbolicLink -Value $target -Force }
+mklink $HOME/dotfiles/.config/vim $HOME/.config/vim
+mklink $HOME/dotfiles/.config/poshthemes $HOME/.config/poshthemes
+mklink $HOME/dotfiles/.config/thefuck $HOME/.config/thefuck
+mklink $HOME/dotfiles/.config/colorls $HOME/.config/colorls
+mklink $HOME/dotfiles/.config/nvim/init.vim $HOME/Appdata/Local/nvim/init.vim
+mklink $HOME/dotfiles/.vimrc $HOME/.vimrc
+mklink $HOME/dotfiles/powershell/Microsoft.PowerShell_profile.ps1 $HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
